80f46cb3b98b508a798a1771cfa42e50c7ebdeb7
lijie <li.jie@uurobot.cn>
2018-08-04 11:00:38 +0800

    支持 客户端发送  前进1米   后退50厘米动作指令

--- U05SdkClientDemo/app/src/main/java/com/canbot/u05/sdk/clientdemo/MainActivity.java
+++ U05SdkClientDemo/app/src/main/java/com/canbot/u05/sdk/clientdemo/MainActivity.java
@@ -439,13 +439,13 @@ public class MainActivity extends Activity implements AdapterView.OnItemClickLis
 
         /**
          * 发送具体动作指令
-         * 例如：后退|后走|向后      数字（1或一） 步    注：一步为0.5米
-         *       前进|前走|向前|走   数字（1或一） 步
-         *       左转|左移|左走      数字（1或一） 度
-         *       右转|右移|右走      数字（1或一） 度
+         * 例如： 后退|后走|向后      + 1米|100厘米"    距离单位支持米和厘米，数值必须为阿拉伯数字，单位名称不能省略
+         *        前进|前走|向前|走   +1米|100厘米  "
+         *        左转|左移|左走      +  90度"
+         *        右转|右移|右走      +  90度"
          */
         public void sendValueMotion() {
-                sendToRobot(MsgType.SEND_CONCRETE_ACTION, "走2步");
+                sendToRobot(MsgType.SEND_CONCRETE_ACTION, "前进50厘米");
         }
 
         /**
