b8a3478a1601c0473313e06d0fc913a7f04e490f
lijie <li.jie@uurobot.cn>
2019-07-17 14:09:56 +0800

    同步 U05 dev分支  sdkdemo

--- U05SdkClientDemo/app/src/main/java/com/canbot/u05/sdk/clientdemo/WebActivity.java
+++ U05SdkClientDemo/app/src/main/java/com/canbot/u05/sdk/clientdemo/WebActivity.java
@@ -1,9 +1,15 @@
 package com.canbot.u05.sdk.clientdemo;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Toast;
 
+import com.canbot.u05.sdk.clientdemo.util.WifiReceiver;
 import com.uurobot.sdkclientdemo.R;
 
 
@@ -11,13 +17,71 @@ public class WebActivity extends Activity {
 
         public static final String TAG = "WebViewActivity";
         private  WebView mWebView;
+        private WifiReceiver wifiReceiver;
+        private IntentFilter wifiIntentFilter;
+
+        /**
+         * Wifi 连接状态改变广播
+         */
+        private WifiReceiver.OnWifiConnectStateChangedListener mWifiListener = new WifiReceiver.OnWifiConnectStateChangedListener() {
+                @Override
+                public void connectedNotU05(String ssid) {
+                        super.connectedNotU05(ssid);
+                        Toast.makeText(WebActivity.this,"外网连接成功：" + ssid,Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void connecting(String ssid) {
+                        super.connecting(ssid);
+                        Log.e("WebActivity", "connecting" + ssid);
+                }
+
+                @Override
+                public void connectedU05(String ssid) {
+                        super.connectedU05(ssid);
+                        Toast.makeText(WebActivity.this,"U05内网连接成功：" + ssid,Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void connectedFailed() {
+                        super.connectedFailed();
+                        Log.e("WebActivity", "connectedFailed");
+                        Toast.makeText(WebActivity.this,"网络连接失败",Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void disconnected() {
+                        super.disconnected();
+                        Toast.makeText(WebActivity.this,"网络已断开",Toast.LENGTH_SHORT).show();
+                }
+        };
+
+
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.activity_web);
-                initWebView();
         }
+
+        @Override
+        protected void onResume() {
+                super.onResume();
+                initWebView();
+                wifiReceiver = new WifiReceiver();
+                wifiIntentFilter = new IntentFilter();
+                wifiIntentFilter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+                wifiIntentFilter.addAction(WifiManager.SUPPLICANT_STATE_CHANGED_ACTION);
+                registerReceiver(wifiReceiver, wifiIntentFilter);
+                WifiReceiver.addOnWifiConnectStateChangedListener(mWifiListener);
+        }
+
+        @Override
+        protected void onPause() {
+                super.onPause();
+                WifiReceiver.removeOnWifiConnectStateChangedListener(mWifiListener);
+        }
+
         /**
          * 初始化webView
          */
